6ebcecbeaa88706712d303a2de43e18080f4b54c
Chris Greeff <chris.greeff@vendhq.com>
2014-04-03 21:01:10 +1300

    Adding javascript transitions for each section

--- web/js/main.js
+++ web/js/main.js
@@ -1,1 +1,12 @@
-alert('well done!');
+(function () {
+    'use strict';
+
+    $('.easyir-main').on('click', '.js-expand', function(event) {
+        var targetNode = $(this),
+            expandableNode = targetNode.parent().find('.easyir-expandable');
+
+        expandableNode.toggleClass('easyir-hidden');
+
+        event.preventDefault();
+    });
+}());
