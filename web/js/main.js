cba90736cb1a1cc17631292f23c2fc125fea8216
Chris Greeff <chris.greeff@vendhq.com>
2014-04-05 18:30:43 +1300

    Tidying up code

--- web/js/main.js
+++ web/js/main.js
@@ -1,21 +1,37 @@
 (function () {
     'use strict';
 
-    // ============================= Changing Slides =============================
-
+    /**
+     * Gets the current slide node that is visible in the viewport.
+     *
+     * @method getSlideNode
+     * @return {Node} The current slide node that is visible in the viewport.
+     */
     function getSlideNode() {
         var mainNode = $('.easyir-main'),
-        storyClass   = getCurrentStoryClass(mainNode),
-        storyNumber  = getCurrentStoryNumber(storyClass);
+        storyClass   = getCurrentClass(mainNode, 'easyir-main'),
+        storyNumber  = getCurrentNumber(storyClass);
 
         return $('#easyir-story' + storyNumber);
     }
 
-    function getCurrentSlideClass(slideNode) {
+    /**
+     * Gets the current slide/story class of the passed node.
+     *
+     * @method getCurrentClass
+     * @param  node {Node}
+     *         The node to get the current slide/story class of.
+     *
+     * @param  skipClass {String}
+     *         The class to skip when searching for the current slide/story class.
+     *
+     * @return {String} The current slide/story class of the passed node.
+     */
+    function getCurrentClass(node, skipClass) {
         var currentClass;
 
-        $.each(slideNode.attr('class').split(' '), function (index, className) {
-            if (className !== 'easyir-story') {
+        $.each(node.attr('class').split(' '), function (index, className) {
+            if (className !== skipClass && !currentClass) {
                 currentClass = className;
             }
         });
@@ -23,25 +39,76 @@
         return currentClass;
     }
 
-    function getCurrentSlideNumber(currentClass) {
+    /**
+     * Gets the current slide/story number of the passed class.
+     *
+     * @method getCurrentNumber
+     * @param  currentClass {String}
+     *         The class to get the current slide/story number of.
+     *
+     * @return {Number} The current slide/story number of the passed class.
+     */
+    function getCurrentNumber(currentClass) {
         var classLength;
 
         classLength = currentClass.length;
-
         return Number(currentClass.substring(classLength - 1, classLength));
     }
 
+    /**
+     * Increments the slide/story number of the passed node
+     *
+     * @method incrementNodeNumber
+     * @param  node {Node}
+     *         The node to increment the current slide/story number on.
+     *
+     * @param  number {Number}
+     *         The current slide/story number of the passed node
+     *
+     * @param  currentClass {String}
+     *         The current slide/story class of the passed node.
+     *
+     * @param  className {String}
+     *         The current slide/story class prefix (i.e. the class name without the slide/story number).
+     */
+    function incrementNodeNumber(node, number, currentClass, className) {
+        node.removeClass(currentClass);
+        number += 1;
+        node.addClass(className + number);
+    }
+
+    /**
+     * Decrements the slide/story number of the passed node
+     *
+     * @method decrementNodeNumber
+     * @param  node {Node}
+     *         The node to decrement the current slide/story number on.
+     *
+     * @param  number {Number}
+     *         The current slide/story number of the passed node
+     *
+     * @param  currentClass {String}
+     *         The current slide/story class of the passed node.
+     *
+     * @param  className {String}
+     *         The current slide/story class prefix (i.e. the class name without the slide/story number).
+     */
+    function decrementNodeNumber(node, number, currentClass, className) {
+        node.removeClass(currentClass);
+        number -= 1;
+        node.addClass(className + number);
+    }
+
+    // ============================= Changing Slides =============================
+
+
     $('.easyir-slide-btn-prev').on('click', function (event) {
         var slideNode    = getSlideNode(),
-            currentClass = getCurrentSlideClass(slideNode),
-            slideNumber  = getCurrentSlideNumber(currentClass);
+            currentClass = getCurrentClass(slideNode, 'easyir-story'),
+            slideNumber  = getCurrentNumber(currentClass);
 
         if (slideNumber > 1) {
-            slideNode.removeClass(currentClass);
-
-            slideNumber -= 1;
-
-            slideNode.addClass('easyir-story-slide' + slideNumber);
+            incrementNodeNumber(slideNode, slideNumber, currentClass, 'easyir-story-slide');
         }
 
         event.preventDefault();
@@ -50,15 +117,11 @@
     $('.easyir-slide-btn-next').on('click', function (event) {
         var slideNode    = getSlideNode(),
             slides       = slideNode.data('slides'),
-            currentClass = getCurrentSlideClass(slideNode),
-            slideNumber  = getCurrentSlideNumber(currentClass);
+            currentClass = getCurrentClass(slideNode, 'easyir-story'),
+            slideNumber  = getCurrentNumber(currentClass);
 
         if (slideNumber < slides) {
-            slideNode.removeClass(currentClass);
-
-            slideNumber += 1;
-
-            slideNode.addClass('easyir-story-slide' + slideNumber);
+            incrementNodeNumber(slideNode, slideNumber, currentClass, 'easyir-story-slide');
         }
 
         event.preventDefault();
@@ -66,54 +129,26 @@
 
     // ============================= Changing Story =============================
 
-    function getCurrentStoryClass(slideNode) {
-        var currentClass;
-
-        $.each(slideNode.attr('class').split(' '), function (index, className) {
-            if (className !== 'easyir-story') {
-                currentClass = className;
-            }
-        });
-
-        return currentClass;
-    }
-
-    function getCurrentStoryNumber(currentClass) {
-        var classLength;
-
-        classLength = currentClass.length;
-
-        return Number(currentClass.substring(classLength - 1, classLength));
-    }
-
     $('.easyir-story-btn-prev').on('click', function (event) {
-        var storyNode    = $('.easyir-main'),
-            currentClass = getCurrentStoryClass(storyNode),
-            storyNumber  = getCurrentStoryNumber(currentClass);
+        var mainNode     = $('.easyir-main'),
+            currentClass = getCurrentClass(mainNode, 'easyir-main'),
+            storyNumber  = getCurrentNumber(currentClass);
 
         if (storyNumber > 1) {
-            storyNode.removeClass(currentClass);
-
-            storyNumber -= 1;
-
-            storyNode.addClass('easyir-main-story' + storyNumber);
+            decrementNodeNumber(mainNode, storyNumber, currentClass, 'easyir-main-story');
         }
 
         event.preventDefault();
     });
 
     $('.easyir-story-btn-next').on('click', function (event) {
-        var storyNode    = $('.easyir-main'),
-            stories      = storyNode.data('stories'),
-            currentClass = getCurrentStoryClass(storyNode),
-            storyNumber  = getCurrentStoryNumber(currentClass);
+        var mainNode     = $('.easyir-main'),
+            stories      = mainNode.data('stories'),
+            currentClass = getCurrentClass(mainNode, 'easyir-main'),
+            storyNumber  = getCurrentNumber(currentClass);
 
         if (storyNumber < stories) {
-            storyNode.removeClass(currentClass);
-
-            storyNumber += 1;
-
-            storyNode.addClass('easyir-main-story' + storyNumber);
+            incrementNodeNumber(mainNode, storyNumber, currentClass, 'easyir-main-story');
         }
 
         event.preventDefault();
